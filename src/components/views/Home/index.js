3c57b4b5783b3a27e5fcf27b06c768279a6afcdc
Manpreet Singh <manpreet.singh@cogneesol.com>
2024-09-23 18:00:26 +0530

    Modal

--- src/components/views/Home/index.js
+++ src/components/views/Home/index.js
@@ -1,90 +1,204 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { RxLapTimer } from "react-icons/rx";
 import { FiPlusCircle } from "react-icons/fi";
 import { message } from "antd";
+import moment from "moment";
+
 
 export default function Home() {
-  const saveLocalStorage = JSON.parse(localStorage.getItem("CreateTaskModal")) || [];
-  const [error, setError] = useState({});
+  //Create New Section Code Start
+  const sectionLocalStorage =
+    JSON.parse(localStorage.getItem("sections")) || [];
 
-  const [addTask, setAddTask] = useState(saveLocalStorage);
-console.log("Addtask", addTask)
+  const [newSection, setNewSection] = useState(sectionLocalStorage);
+
+  const [newSectionError, setNewSectionError] = useState({});
+
+  console.log("sections", newSection);
+
+  //AddTask Const
+  
+ 
 
   const [formState, setFormState] = useState({
-    taskTitle: "",
-    taskDesc: "",
-    taskDate: "",
-    taskTime: "",
-    taskPriority: "",
-    taskMedia: [],
+    sectionTitle: "",
   });
 
-  console.log("formState", formState);
-  console.log("error", error);
-
-  const handleFileUpload = (index, event) => {
-  const files = event.target.files;
-  if (files.length && files.length > 1 ) {
-    setError({});
-    message.error("`Only one file can be uploaded. Allowed file type: .jpg, .png, max size: 10MB.`")
-  } 
-
-
-
-  }
-
-  const handleSubmit = (e) => {
+  const sectionHandle = (e) => {
     e.preventDefault();
-    if (
-      formState?.taskTitle &&
-      formState?.taskDesc &&
-      formState?.taskDate &&
-      formState?.taskPriority &&
-      formState?.taskMedia.length > 0
-    ) {
-      const addModalTask = {
-        taskTitle: formState?.taskTitle,
-        taskDesc: formState?.taskDesc,
-        taskDate: formState?.taskDate,
-        taskPriority: formState?.taskPriority,
-        taskMedia: formState?.taskMedia,
+    if (formState?.sectionTitle) {
+      const addNewSection = {
+        sectionId: newSection.length + 1,
+        sectionTitle: formState?.sectionTitle,
       };
-      const updatedTasks = [...saveLocalStorage, addModalTask];
-      localStorage.setItem("CreateTaskModal", JSON.stringify(updatedTasks));
-      setAddTask(updatedTasks);
+      const updateSection = [...newSection, addNewSection];
+      localStorage.setItem("sections", JSON.stringify(updateSection));
+      setNewSection(updateSection);
       setFormState({
-        taskTitle: "",
-        taskDesc: "",
-        taskDate: "",
-        taskPriority: "",
+        sectionTitle: "",
       });
-      setError({});
+      setNewSectionError({});
     } else {
-      setError({
-        taskTitle: formState?.taskTitle ? "" : "Title is Mandatory",
-        taskDesc: formState?.taskDesc ? "" : "Descripction is Mandatory",
-        taskDate: formState?.taskDate ? "" : "Date is Mandatory",
-        taskMedia: formState?.taskMedia.length > 0 ? "" : "At least one file is mandatory",
+      setNewSectionError({
+        sectionTitle: formState?.sectionTitle ? "" : "Title is Mandatory",
       });
     }
   };
 
+  //Create New Section Code End
+
+  const [addTask, setAddTask] = useState([]);
+
+  //Create New task Code Start
+
+const handleTask = (e,sectionId) => {
+  e.preventDefault()
+  const allSections = [...newSection];
+  
+  allSections && allSections.map((item,index) => {
+    console.log(sectionId,' === ',item?.sectionId);
+    if(sectionId === item?.sectionId){
+      const newTask = {
+        taskTitle: "",
+        taskDesc: "",
+        taskDate: "",
+        taskPriority: "",
+        //taskMedia : [],
+      };
+     
+
+      
+      const updateTask = [...addTask, newTask];
+      setAddTask(updateTask);
+      item.tasks=updateTask; 
+    }
+  });
+  setNewSection(allSections);
+
+
+
+  console.log("addtask",newSection);
+
+
+}
+
+
+
+  // // Set state of pictures
+  // const [pictures, setPictures] = useState([
+  //   {
+  //     data: [],
+  //     url: "",
+  //   },
+  // ]);
+
+  // // Function to display pictures
+  // const handleImageUpload = (e) => {
+  //   const tempArr = [];
+  //   [...e.target.files].forEach((file) => {
+  //     tempArr.push({
+  //       data: file,
+  //       url: URL.createObjectURL(file),
+  //     });
+  //   });
+  //   setPictures(tempArr);
+  // };
+
+  //Create New task Code End
+
   return (
     <div className="taskcreate p-4">
       <div className="row">
-        <div className="col-sm-4">
-          <h5>To Do</h5>
-          <button
-            className="addbutton"
-            data-toggle="modal"
-            data-target="#exampleModal"
-          >
-            <FiPlusCircle className="mr-2" /> Add task
-          </button>
-          {/** Modal**/}
-          <div
+        <div
+          className="modal fade"
+          id="newsection"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="newsection"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Create New Section</h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body p-3">
+                <form>
+                  <div className="col-sm-12 p-0 mb-3">
+                    <label>
+                      Section Title <span className="text-danger">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      className={
+                        newSectionError?.sectionTitle
+                          ? "invalid form-control"
+                          : "form-control"
+                      }
+                      value={formState?.sectionTitle}
+                      onChange={(e) => {
+                        setFormState({
+                          ...formState,
+                          sectionTitle: e.target.value,
+                        });
+                      }}
+                    />
+                    <small className="text-danger">
+                      {newSectionError?.sectionTitle}
+                    </small>
+                  </div>
+
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      data-dismiss="modal"
+                    >
+                      Close
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-primary"
+                      data-dismiss={formState?.sectionTitle ? "modal" : ""}
+                      onClick={sectionHandle}
+                    >
+                      Save changes
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {newSection &&
+          newSection?.map((data, index,) => {
+            const {sectionId} = data;
+      
+            return (
+              <>
+              <div className="col-sm-4" key={index}>
+                <h5>{data?.sectionTitle}</h5>
+                <button
+                  className="addbutton"
+                  data-toggle="modal"
+                  data-target={`#exampleModal${sectionId}`}
+                >
+                  <FiPlusCircle className="mr-2" /> Add task
+                </button>
+              </div>
+                  {/**Add Task Modal**/}
+                  <div
             className="modal fade"
-            id="exampleModal"
+            id={`exampleModal${sectionId}`}
             tabIndex="-1"
             role="dialog"
             aria-labelledby="exampleModalLabel"
@@ -108,53 +222,51 @@ console.log("Addtask", addTask)
                 <div className="modal-body p-3">
                   <form>
                     <div className="col-sm-12 p-0 mb-3">
-                      <label>Title <span className="text-danger">*</span></label>
+                      <label>Title</label>
                       <input
                         type="text"
-                        className={
-                          error?.taskTitle
-                            ? "invalid form-control"
-                            : "form-control"
-                        }
-                        value={formState?.taskTitle}
+                        // className={
+                        //   error?.taskTitle
+                        //     ? "invalid form-control"
+                        //     : "form-control"
+                        // }
                         onChange={(e) => {
                           setFormState({
                             ...formState,
                             taskTitle: e.target?.value,
                           });
-                          setError({
-                            ...error,
-                            taskTitle: e.target?.value
-                              ? ""
-                              : "Title is Mandatory",
-                          });
+                          // setError({
+                          //   ...error,
+                          //   taskTitle: e.target?.value
+                          //     ? ""
+                          //     : "Title is Mandatory",
+                          // });
                         }}
                       />
-                      <small className="text-danger">{error?.taskTitle}</small>
+                      {/* <small className="text-danger">{error?.taskTitle}</small> */}
                     </div>
                     <div className="col-sm-12 p-0 mb-3">
-                      <label>Description <span className="text-danger">*</span></label>
+                      <label>Description</label>
                       <textarea
-                        className={
-                          error?.taskDesc
-                            ? "invalid form-control"
-                            : "form-control"
-                        }
-                        value={formState?.taskDesc}
+                        // className={
+                        //   error?.taskDesc
+                        //     ? "invalid form-control"
+                        //     : "form-control"
+                        // }
                         onChange={(e) => {
                           setFormState({
                             ...formState,
                             taskDesc: e.target?.value,
                           });
-                          setError({
-                            ...error,
-                            taskDesc: e.target?.value
-                              ? ""
-                              : "Description is Mandatory",
-                          });
+                          // setError({
+                          //   ...error,
+                          //   taskDesc: e.target?.value
+                          //     ? ""
+                          //     : "Description is Mandatory",
+                          // });
                         }}
                       />
-                      <small className="text-danger">{error?.taskDesc}</small>
+                      {/* <small className="text-danger">{error?.taskDesc}</small> */}
                     </div>
                     <div className="col-sm-12 p-0 mb-3">
                       <label htmlFor="exampleFormControlFile1">
@@ -162,18 +274,17 @@ console.log("Addtask", addTask)
                       </label>
                       <select
                         className="form-control"
-                        value={formState?.taskPriority}
                         onChange={(e) => {
                           setFormState({
                             ...formState,
                             taskPriority: e.target?.value,
                           });
-                          setError({
-                            ...error,
-                            taskPriority: e.target?.value
-                              ? ""
-                              : "Priority is Mandatory",
-                          });
+                          // setError({
+                          //   ...error,
+                          //   taskPriority: e.target?.value
+                          //     ? ""
+                          //     : "Priority is Mandatory",
+                          // });
                         }}
                       >
                         <option value="">Select Priority</option>
@@ -181,48 +292,34 @@ console.log("Addtask", addTask)
                         <option>Med</option>
                         <option>High</option>
                       </select>
-                      <small className="text-danger">
+                      {/* <small className="text-danger">
                         {error?.taskPriority}
-                      </small>
+                      </small> */}
                     </div>
 
                     <div className="col-sm-12 p-0 mb-3">
-                      <label className="d-block ">Task End Date <span className="text-danger">*</span></label>
+                      <label className="d-block ">Task End Date</label>
                       <input
                         type="date"
-                        className={
-                          error?.taskDate
-                            ? "invalid form-control"
-                            : "form-control"
-                        }
-                        value={formState?.taskDate}
+                        // className={
+                        //   error?.taskDate
+                        //     ? "invalid form-control"
+                        //     : "form-control"
+                        // }
                         onChange={(e) => {
                           setFormState({
                             ...formState,
                             taskDate: e.target?.value,
                           });
-                          setError({
-                            ...error,
-                            taskDate: e.target?.value
-                              ? ""
-                              : "Date is Mandatory",
-                          });
+                          // setError({
+                          //   ...error,
+                          //   taskDate: e.target?.value
+                          //     ? ""
+                          //     : "Date is Mandatory",
+                          // });
                         }}
                       />
-                      <small className="text-danger">{error?.taskDate}</small>
-                    </div>
-
-                    <div className="col-sm-12 p-0 mb-3">
-                      <label htmlFor="exampleFormControlFile1">
-                        Upload Documents <span className="text-danger">*</span>
-                      </label>
-                      <input
-                        type="file"
-                        multiple
-                        className="form-control-file form-control"
-                        id="exampleFormControlFile1"
-                     />
-                      <small className="text-danger">{error?.taskMedia}</small>
+                      {/* <small className="text-danger">{error?.taskDate}</small> */}
                     </div>
 
                     <div className="modal-footer">
@@ -234,18 +331,11 @@ console.log("Addtask", addTask)
                         Close
                       </button>
                       <button
-                        type="button"
+                        type="submit"
                         className="btn btn-primary"
-                        data-dismiss={
-                          formState?.taskTitle &&
-                          formState?.taskDesc &&
-                          formState?.taskDate &&
-                          formState?.taskPriority &&
-                          formState?.taskMedia 
-                            ? "modal"
-                            : ""
-                        }
-                        onClick={handleSubmit}
+                        onClick={(e) =>{
+                          handleTask(e,sectionId)
+                        }}
                       >
                         Save changes
                       </button>
@@ -255,29 +345,22 @@ console.log("Addtask", addTask)
               </div>
             </div>
           </div>
-          {/** Modal**/}
-
-          {/**Card Start*/}
-          {addTask.map((task, index) => {
-            return (
-              <div className="todo mb-3" key={index}>
-                <div className="risk mb-3 d-flex justify-content-between">
-                  <span className={`bu ${task?.taskPriority.toLowerCase()}-btn`}>{task?.taskPriority}</span>
-                  <RxLapTimer />
-                </div>
-                <div className="todocards mb-3">
-                  <p className="m-0">{task?.taskTitle}</p>
-                  <p className="m-0">{task?.taskDesc}</p>
-                </div>
-                <div className="assign-area  d-flex justify-content-between">
-                  <img src="https://c8.alamy.com/comp/2RCTH5W/img-logo-img-letter-img-letter-logo-design-initials-img-logo-linked-with-circle-and-uppercase-monogram-logo-img-typography-for-technology-busines-2RCTH5W.jpg" />
-                  <p className="m-0">Task End Date : {task?.taskDate}</p>
-                </div>
-              </div>
+          {/** Add Task Modal**/}
+          </>
             );
           })}
 
-          {/**Card Start*/}
+
+
+        <div className="col-sm-4">
+          <h5>Create Section</h5>
+          <button
+            className="addbutton"
+            data-toggle="modal"
+            data-target="#newsection"
+          >
+            <FiPlusCircle className="mr-2" /> Create New Section
+          </button>
         </div>
       </div>
     </div>
